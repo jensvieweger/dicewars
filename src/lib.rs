65978eeff68a9d57d60144b1b98608f557a19fd6
Jens Vieweger <jens.vieweger@gmx.de>
2017-03-02 12:48:43 +0100

    [Added] Code style
     - via plain rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -11,13 +11,18 @@ pub enum Faction {
     /// Field is blocked., i.e. can't be occupied.
     Blocked,
     /// ID of the Player occupying the field.
-    Player{ id: u8},
+    Player { id: u8 },
 }
 
 impl Faction {
     pub fn new(max_players: u8) -> Faction {
-        let rng_result = rand::thread_rng().gen_range(0, if max_players == 255 {max_players} else { max_players + 1 });
-        
+        let rng_result = rand::thread_rng().gen_range(0,
+                                                      if max_players == 255 {
+                                                          max_players
+                                                      } else {
+                                                          max_players + 1
+                                                      });
+
         if max_players >= 255 {
             if rand::thread_rng().gen_range(0, 256) == 0 {
                 Faction::Blocked
@@ -30,7 +35,7 @@ impl Faction {
             } else {
                 Faction::Player { id: rng_result - 1 }
             }
-        }     
+        }
     }
 }
 
@@ -38,18 +43,21 @@ impl Faction {
 pub struct Field {
     /// Stores the ID of the faction currently occupying this field.
     faction: Faction,
-    
+
     /// Stores the number of dices on this field.
     num_dices: u8,
 }
 
 impl Field {
     pub fn new(max_players: u8, max_dices: u8) -> Field {
-        Field { faction: Faction::new(max_players), num_dices: rand::thread_rng().gen_range(0, max_dices+1)}
+        Field {
+            faction: Faction::new(max_players),
+            num_dices: rand::thread_rng().gen_range(0, max_dices + 1),
+        }
     }
 }
 
-/// Describes the shape of the fields, i.e. how many direct neighbours a field has 
+/// Describes the shape of the fields, i.e. how many direct neighbours a field has
 pub enum Shape {
     Square,
     //Hexagon,
@@ -58,10 +66,10 @@ pub enum Shape {
 pub struct Map {
     /// How many fields along the X-Axis
     x_size: u8,
-    
+
     /// How many fields along the Y-Axis
     y_size: u8,
-    
+
     /// The actual map of the game.
     fields: Vec<Vec<Field>>,
 }
@@ -73,20 +81,24 @@ impl Map {
         assert_ne!(x_size, 0);
         assert_ne!(y_size, 0);
 
-        let mut fields:Vec<Vec<Field>> = Vec::new();
-        for _ in 0..x_size-1 {
-            let mut column:Vec<Field> = Vec::new();
-            for _ in 0..y_size-1 {
+        let mut fields: Vec<Vec<Field>> = Vec::new();
+        for _ in 0..x_size - 1 {
+            let mut column: Vec<Field> = Vec::new();
+            for _ in 0..y_size - 1 {
                 column.push(Field::new(max_players, max_dices));
             }
             fields.push(column);
         }
-        
-        let mut map:Map = Map { x_size: x_size, y_size: y_size, fields:fields }; 
+
+        let mut map: Map = Map {
+            x_size: x_size,
+            y_size: y_size,
+            fields: fields,
+        };
         map.sanitize_map();
         map
     }
-    
+
     /// after initial generation, this function makes sure that the generated map is actually playable
     /// here: all non-blocked fields must be connected
     fn sanitize_map(&mut self) -> &mut Map {
@@ -94,7 +106,7 @@ impl Map {
         self.fields[0][0].num_dices = 1;
         self
     }
-    
+
     pub fn pretty_print(&self) {
         let borders = "╔═╗║╝╚╟╢╤╧│─┼";
         // ╔═╤═╤═╤═╤═╗
@@ -102,12 +114,18 @@ impl Map {
         // ╟─┼─┼─┼─┼─╢
         // ║2│4│6│-│-║
         // ╚═╧═╧═╧═╧═╝
-        
-        for y in 0..self.y_size-1 {
-            for x in 0..self.x_size-1 {
+
+        for y in 0..self.y_size - 1 {
+            for x in 0..self.x_size - 1 {
                 match self.fields[x as usize][y as usize].faction {
                     Faction::Blocked => print!("{}     ", Red.paint("-")),
-                    Faction::Player{id: id} => print!("{} ({}) ", Green.paint(self.fields[x as usize][y as usize].num_dices.to_string()), id),                   
+                    Faction::Player { id: id } => {
+                        print!("{} ({}) ",
+                               Green.paint(self.fields[x as usize][y as usize]
+                                   .num_dices
+                                   .to_string()),
+                               id)
+                    }
                 };
             }
             println!("");
@@ -119,16 +137,16 @@ impl Map {
 pub struct Game {
     /// How many players are part of the game
     num_players: u8,
-    
+
     /// The maximal number of dices per field
     max_dices: u8,
-    
+
     /// Stores the map of the game
-    map:Map,
-    
+    map: Map,
+
     /// Which round is it currently
     round: usize,
-    
+
     /// whose turn it currently is
     turn: Faction,
 }
@@ -140,14 +158,14 @@ impl Game {
             max_dices: max_dices,
             map: Map::new(max_players, max_dices, x_size, y_size),
             round: 0,
-            turn: Faction::Blocked
+            turn: Faction::Blocked,
         }
     }
-    
+
     pub fn get_dices_of_first_field(&self) -> u8 {
         self.map.fields[0][0].num_dices
     }
-    
+
     pub fn print(&self) {
         self.map.pretty_print();
     }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ use dicewars::Game;
 
 fn main() {
     println!("Welcome to DiceWars!");
-    let game:Game = Game::new(2, 6, 6, 4);
-    
-    println!("number of dices on the first fields: {}",game.get_dices_of_first_field());
+    let game: Game = Game::new(2, 6, 6, 4);
+
+    println!("number of dices on the first fields: {}",
+             game.get_dices_of_first_field());
     game.print();
 }
