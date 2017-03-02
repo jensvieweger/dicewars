65bbed6dbb48e7daa25c704b2349cb4738a0caf1
Jens Vieweger <jens.vieweger@gmx.de>
2017-03-02 22:22:05 +0100

    [Added] printing of the map [Fixed] Typos

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ extern crate ansi_term;
 
 use rand::Rng;
 use ansi_term::Colour::*;
+//use ansi_term::Style;
 
 
 /// Enum holding information about the type of a field.
@@ -49,15 +50,15 @@ pub struct Field {
     /// Stores the ID of the faction currently occupying this field.
     faction: Faction,
 
-    /// Stores the number of dices on this field.
-    num_dices: u8,
+    /// Stores the number of dice on this field.
+    num_dice: u8,
 }
 
 impl Field {
-    pub fn new(max_players: u8, max_dices: u8) -> Field {
+    pub fn new(max_players: u8, max_dice: u8) -> Field {
         Field {
             faction: Faction::new(max_players),
-            num_dices: rand::thread_rng().gen_range(0, max_dices + 1),
+            num_dice: rand::thread_rng().gen_range(0, max_dice + 1),
         }
     }
 }
@@ -79,19 +80,19 @@ pub struct Map {
 }
 
 impl Map {
-    pub fn new(max_players: u8, max_dices: u8, x_size: u8, y_size: u8) -> Map {
+    pub fn new(max_players: u8, max_dice: u8, x_size: u8, y_size: u8) -> Map {
         assert_ne!(max_players, 0);
-        assert_ne!(max_dices, 0);
+        assert_ne!(max_dice, 0);
         assert_ne!(x_size, 0);
         assert_ne!(y_size, 0);
 
         let mut fields: Vec<Vec<Field>> = Vec::new();
-        for _ in 0..x_size - 1 {
-            let mut column: Vec<Field> = Vec::new();
-            for _ in 0..y_size - 1 {
-                column.push(Field::new(max_players, max_dices));
+        for _ in 0..y_size {
+            let mut row: Vec<Field> = Vec::new();
+            for _ in 0..x_size {
+                row.push(Field::new(max_players, max_dice));
             }
-            fields.push(column);
+            fields.push(row);
         }
 
         let mut map: Map = Map {
@@ -107,7 +108,8 @@ impl Map {
     /// here: all non-blocked fields must be connected and each player must have ~ the same amount of dice
     fn sanitize_map(&mut self) -> &mut Map {
         /* FIXME: implement */
-        self.fields[0][0].num_dices = 1;
+        //self.fields[0][0].num_dice = 1;
+        //println!("{} {}", self.fields.len(), self.fields[0].len());
         self
     }
 
@@ -119,22 +121,61 @@ impl Map {
     /// ╚═╧═╧═╧═╧═╝
     ///
     pub fn pretty_print(&self) {
+        // header
+        print!("╔");
+        for _ in 0..self.fields[0].len() - 1 {
+            print!("═╤");
+        }
+        println!("═╗");
 
-        for y in 0..self.y_size - 1 {
-            for x in 0..self.x_size - 1 {
-                match self.fields[x as usize][y as usize].faction {
-                    Faction::Blocked => print!("{}     ", Red.paint("-")),
-                    Faction::Player { id: id } => {
-                        print!("{} ({}) ",
-                               Green.paint(self.fields[x as usize][y as usize]
-                                   .num_dices
-                                   .to_string()),
-                               id)
+        let mut row_iter = self.fields.iter().peekable();
+        let mut row = row_iter.next().expect("Invalid map (no row available)");
+
+        loop {
+            print!("║");
+
+            let mut field_iter = row.iter().peekable();
+            let mut field = field_iter.next().expect("Invalid map (row is empty)");
+
+            loop {
+                match field.faction {
+                    Faction::Blocked => print!("{}", Fixed(255).bold().paint("-")),
+                    Faction::Player { id } => {
+                        //print!("{} ({}) ", Green.paint(field.num_dice.to_string()), id)
+                        print!("{}", Fixed(id).paint(field.num_dice.to_string()))
                     }
                 };
+                match field_iter.peek() {
+                    Some(_) => print!("│"),
+                    None => {
+                        println!("║");
+                        break;
+                    }
+                }
+                field = field_iter.next().expect("Invalid map (row is empty)");
             }
-            println!("");
+
+            match row_iter.peek() {
+                Some(_) => {
+                    // between the lines
+                    print!("╟");
+                    for _ in 0..self.fields[0].len() - 1 {
+                        print!("─┼");
+                    }
+                    println!("─╢");
+                }
+                None => break,
+            }
+            row = row_iter.next().expect("Invalid map (no row available)");
         }
+
+        // footer
+        print!("╚");
+        for _ in 0..self.fields[0].len() - 1 {
+            print!("═╧");
+        }
+        println!("═╝");
+
     }
 }
 
@@ -144,7 +185,7 @@ pub struct Game {
     num_players: u8,
 
     /// The maximal number of dices per field
-    max_dices: u8,
+    max_dice: u8,
 
     /// Stores the map of the game
     map: Map,
@@ -157,21 +198,26 @@ pub struct Game {
 }
 
 impl Game {
-    pub fn new(max_players: u8, max_dices: u8, x_size: u8, y_size: u8) -> Game {
+    pub fn new(max_players: u8, max_dice: u8, x_size: u8, y_size: u8) -> Game {
         Game {
             num_players: max_players,
-            max_dices: max_dices,
-            map: Map::new(max_players, max_dices, x_size, y_size),
+            max_dice: max_dice,
+            map: Map::new(max_players, max_dice, x_size, y_size),
             round: 0,
-            turn: Faction::Blocked,
+            turn: Faction::Player { id: 1 },
         }
     }
 
-    pub fn get_dices_of_first_field(&self) -> u8 {
-        self.map.fields[0][0].num_dices
-    }
-
     pub fn print(&self) {
+        match self.turn {
+            Faction::Blocked => panic!("the turn must be on a valid faction"),
+            Faction::Player { id } => {
+                println!("Player: {}/{}, round: {}",
+                         Fixed(id).paint(id.to_string()),
+                         self.num_players,
+                         self.round);
+            }
+        }
         self.map.pretty_print();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ use dicewars::Game;
 
 fn main() {
     println!("Welcome to DiceWars!");
-    let game: Game = Game::new(2, 6, 6, 4);
+    let game: Game = Game::new(8, 6, 10, 6);
 
-    println!("number of dices on the first fields: {}",
-             game.get_dices_of_first_field());
     game.print();
 }
